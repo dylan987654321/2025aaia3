4e6a8157f3d16cc7d9b73d92866981b5518b8376
dylan987654321 <liushaomin950225@gmail.com>
2025-11-13 17:22:19 +0800

    week10

---
+++ week10/week10-1a.cpp
@@ -0,0 +1,13 @@
+//week10-1a.cpp寫兩次,第一次,用for迴圈,第二次用數學
+//1523.Count Odd Numbers in an Interval Range
+//從low ... high裡面有幾個od奇數
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int  ans = 0;
+        for (int i=low; i<=high; i++){
+            if (i%2==1) ans++;
+        }
+        return ans;
+    }
+};
